3c2e54610b3bc5bb93f45d9cab8b82abd1eac519
Daviidg <davidgilif@gmail.com>
2026-06-18 13:54:41 +0200

    generic om plugin changes

--- catalog/internal/plugins/config.go
+++ catalog/internal/plugins/config.go
@@ -4,10 +4,12 @@ import (
 	"github.com/naira-project/naira/catalog/internal/plugins/depl_calls_svc"
 	"github.com/naira-project/naira/catalog/internal/plugins/litellm"
 	"github.com/naira-project/naira/catalog/internal/plugins/mlflow"
+	"github.com/naira-project/naira/catalog/internal/plugins/openmetadata"
 )
 
 type Config struct {
 	MLflow       mlflow.Config         `env:"MLFLOW_"`
 	LiteLLM      litellm.Config        `env:"LITELLM_"`
 	DeplCallsSvc depl_calls_svc.Config `env:"DEPL_CALLS_SVC_"`
+	OpenMetadata openmetadata.Config   `env:"OPENMETADATA_"`
 }
--- catalog/internal/plugins/plugins.go
+++ catalog/internal/plugins/plugins.go
@@ -7,6 +7,7 @@ import (
 	"github.com/naira-project/naira/catalog/internal/plugins/depl_calls_svc"
 	"github.com/naira-project/naira/catalog/internal/plugins/litellm"
 	"github.com/naira-project/naira/catalog/internal/plugins/mlflow"
+	"github.com/naira-project/naira/catalog/internal/plugins/openmetadata"
 	"github.com/naira-project/naira/catalog/pluginapi"
 )
 
@@ -23,6 +24,10 @@ func Register(config Config, httpClient *http.Client, logger *log.Logger) []plug
 
 	if config.DeplCallsSvc.Enabled {
 		registered = appendIfNotNil(registered, depl_calls_svc.New(config.DeplCallsSvc))
+	}
+
+	if config.OpenMetadata.Enabled {
+		registered = appendIfNotNil(registered, openmetadata.New(httpClient, config.OpenMetadata))
 	}
 
 	return registered
