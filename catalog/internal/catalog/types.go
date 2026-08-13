c93e665ef5988509640fb38bd7746ad0d42c877b
libera13 <49229408+libera13@users.noreply.github.com>
2026-08-13 07:54:40 +0200

    Feat: refactors plugin execution from synchronous to asynchronous (#135)
    * feat: plugins table init
    
    * test: init tests
    
    * feat: Dialog for plugins
    
    * feat: connect plugin run with app ctx
    
    * feat: single hook for runall and runone plugin
    
    * feat: sync running state
    
    * fix: import fix
    
    * feat: update each status run
    
    * feat: state of run and run all, are seperated
    
    * feat: remove plugins run history
    
    * feat: show duration in plugins table
    
    * feat: plugin error in dialog
    
    * feat: cleanup PluginErrorModal
    
    * feat: show error btn in  status column
    
    * feat: fix column width
    
    * fix: plugins data was not showing on local
    
    * refactor: one formatRelativeTime func
    
    * chore: remove not used funcs
    
    * chore: remove delete method from operationstore
    
    * chore: pass operationstore as standard argument to newservice
    
    * chore: faster evictOldestForPluginLocked
    
    * chore: remove unneeded err wrapper
    
    * chore: split catalog internals into smaller files
    
    * test: split tests into smaller files
    
    * chore: remove ui-part
    
    * chore: remove ui-part
    
    * chore: remove comment
    
    * chore: add two comments

--- catalog/internal/catalog/types.go
+++ catalog/internal/catalog/types.go
@@ -2,8 +2,6 @@ package catalog
 
 import "github.com/naira-project/naira/plugins/pkg/pluginapi"
 
-type Plugin = pluginapi.Plugin
-
 type PropertyMap = pluginapi.PropertyMap
 
 type NodeID = pluginapi.NodeID
@@ -13,12 +11,3 @@ type CollectResponse = pluginapi.CollectResponse
 type NodeClaim = pluginapi.NodeClaim
 
 type RelationClaim = pluginapi.RelationClaim
-
-type RunPluginResult struct {
-	Plugin string
-	Error  string
-}
-
-type RunPluginsResult struct {
-	Results []RunPluginResult
-}
