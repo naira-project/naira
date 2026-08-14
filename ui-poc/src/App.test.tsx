8a0565cd4692879ee9d30f1e4545dda2ff155d48
libera13 <e.libera@reply.com>
2026-08-14 10:17:14 +0200

    fix: test was logging warns

--- ui-poc/src/App.test.tsx
+++ ui-poc/src/App.test.tsx
@@ -1,7 +1,9 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
 import App from './App';
 
-test('renders the Naira application shell', () => {
-  render(<App />);
+test('renders the Naira application shell', async () => {
+  await act(async () => {
+    render(<App />);
+  });
   expect(screen.queryByText(/learn react/i)).not.toBeInTheDocument();
 });
